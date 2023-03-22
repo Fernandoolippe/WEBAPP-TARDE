4cd2373e9e9107c731c32bacb37fa7bc53ab9823
Alexandre <alexandre.santos141@fatec.sp.gov.br>
2023-03-22 15:08:34 -0300

    Add project files.

---
+++ WEBAPP-TARDE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEBAPP_TARDE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
