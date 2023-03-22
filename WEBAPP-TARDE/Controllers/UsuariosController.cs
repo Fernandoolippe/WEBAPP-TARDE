ce38175d52821c0bf96eb56a7fa5f659d00c7590
Alexandre <alexandre.santos141@fatec.sp.gov.br>
2023-03-22 16:10:07 -0300

    criação do dropdown

---
+++ WEBAPP-TARDE/Controllers/UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPP_TARDE.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+
+            return View();
+
+        }
+            
+     }
+}
